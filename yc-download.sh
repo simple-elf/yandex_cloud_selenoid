b18b7745e92ad820873ac3a9ce0de8680ba0089c
Aaftakhov <simple1elf@gmail.com>
2021-06-28 19:26:10 +0500

    curl jq

--- yc-download.sh
+++ yc-download.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install jq -y
+JQ=/usr/bin/jq
+curl https://stedolan.github.io/jq/download/linux64/jq > $JQ && chmod +x $JQ
+ls -la $JQ
 curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
 source "/var/jenkins_home/.bashrc"
 echo $PATH
 
 #yc --version
 echo "YC_CLOUD_ID: $YC_CLOUD_ID"
+echo "YC_FOLDER_ID: $YC_FOLDER_ID"
 
 yc config set token $YC_TOKEN
 yc config set folder-id $YC_FOLDER_ID
