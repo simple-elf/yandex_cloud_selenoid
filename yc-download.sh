08546b6b3cb8b54813b9a721ac36fb90e7f6f5dd
Aaftakhov <simple1elf@gmail.com>
2021-06-28 17:53:48 +0500

    yc download 2

--- yc-download.sh
+++ yc-download.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
+source "/var/jenkins_home/.bashrc"
+echo $PATH
 export PATH="/home/yandex-cloud/bin:${PATH}"
+echo $PATH
 
 yc --help
 echo "YC_CLOUD_ID: $YC_CLOUD_ID"
