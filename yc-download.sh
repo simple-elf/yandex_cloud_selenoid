d0de89283bbd2fed03c26485a997754ee9e067fd
Aaftakhov <simple1elf@gmail.com>
2021-06-28 17:56:10 +0500

    yc config set

--- yc-download.sh
+++ yc-download.sh
@@ -3,13 +3,10 @@
 curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
 source "/var/jenkins_home/.bashrc"
 echo $PATH
-export PATH="/home/yandex-cloud/bin:${PATH}"
-echo $PATH
 
-yc --help
+#yc --help
 echo "YC_CLOUD_ID: $YC_CLOUD_ID"
 
-yc init --cloud-id $YC_CLOUD_ID --folder-id $YC_FOLDER_ID --token $YC_TOKEN
-#yc config set token $YC_TOKEN
-#yc config set folder-id $YC_FOLDER_ID
-#yc config set cloud-id $YC_CLOUD_ID
+yc config set token $YC_TOKEN
+yc config set folder-id $YC_FOLDER_ID
+yc config set cloud-id $YC_CLOUD_ID
