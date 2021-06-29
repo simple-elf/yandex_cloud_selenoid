2380f8f5054aa15947a83d6680191e956a1cb031
Aaftakhov <simple1elf@gmail.com>
2021-06-29 11:49:41 +0500

    script json 2

--- yc-create.sh
+++ yc-create.sh
@@ -6,10 +6,11 @@ CORE=$1 # TODO second param???
 source "/var/jenkins_home/.bashrc"
 #yc --version
 
-yc compute instance create-with-container \
+# shellcheck disable=SC2046
+echo $(yc compute instance create-with-container \
 --docker-compose-file docker-compose.yml \
 --zone ru-central1-c \
 --public-ip \
 --memory $MEMORY --cores $CORE --core-fraction 100 \
 --service-account-name selenoid \
---format json
+--format json)
--- yc-download.sh
+++ yc-download.sh
@@ -2,11 +2,11 @@
 
 curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
 source "/var/jenkins_home/.bashrc"
-echo $PATH
+#echo $PATH
 
 #yc --version
-echo "YC_CLOUD_ID: $YC_CLOUD_ID"
-echo "YC_FOLDER_ID: $YC_FOLDER_ID"
+#echo "YC_CLOUD_ID: $YC_CLOUD_ID"
+#echo "YC_FOLDER_ID: $YC_FOLDER_ID"
 
 yc config set token $YC_TOKEN
 yc config set folder-id $YC_FOLDER_ID
