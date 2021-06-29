d3346a2675961a214893279155ffe1f305814b11
Aaftakhov <simple1elf@gmail.com>
2021-06-29 11:44:57 +0500

    script json

--- yc-create.sh
+++ yc-create.sh
@@ -4,15 +4,12 @@ MEMORY=$1
 CORE=$1 # TODO second param???
 
 source "/var/jenkins_home/.bashrc"
-yc --version
+#yc --version
 
-INSTANCE_ID=$(yc compute instance create-with-container \
+yc compute instance create-with-container \
 --docker-compose-file docker-compose.yml \
 --zone ru-central1-c \
 --public-ip \
 --memory $MEMORY --cores $CORE --core-fraction 100 \
 --service-account-name selenoid \
---format json | jq .id -r)
-
-export INSTANCE_ID=$INSTANCE_ID
-echo "$INSTANCE_ID"
+--format json
--- yc-download.sh
+++ yc-download.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-JQ=/usr/bin/jq
-curl https://stedolan.github.io/jq/download/linux64/jq > $JQ && chmod +x $JQ
-ls -la $JQ
 curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
 source "/var/jenkins_home/.bashrc"
 echo $PATH
