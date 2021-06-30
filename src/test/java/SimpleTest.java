2a114045d7e00a92a950301caf8852a31080e678
Aaftakhov <simple1elf@gmail.com>
2021-06-30 15:52:58 +0500

    download video

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -4,13 +4,21 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.screenshot;
 import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SimpleTest {
 
@@ -57,6 +65,77 @@ public class SimpleTest {
   @AfterEach
   public void afterEach() {
     step("video");
+    String sessionId = getSessionId();
+    closeWebDriver();
+    attachAllureVideo(sessionId);
+  }
+
+  @Step
+  public static void attachAllureVideo(String sessionId) {
+    try {
+      URL videoUrl = new URL(Configuration.remote + "/video/" + sessionId + ".mp4");
+      step("Video link: " + videoUrl.toString());
+      InputStream is = getSelenoidVideo(videoUrl);
+      Allure.addAttachment("Video", "video/mp4", is, "mp4");
+      System.out.println("attachAllureVideo attachment");
+      deleteSelenoidVideo(videoUrl);
+    } catch (Exception e) {
+      System.out.println("attachAllureVideo exception");
+      e.printStackTrace();
+    }
+  }
+
+  @Step
+  public static InputStream getSelenoidVideo(URL url) {
+    int lastSize = 0;
+    int exit = 2;
+    for (int i = 0; i < 20; i++) {
+      try {
+        int size = Integer.parseInt(url.openConnection().getHeaderField("Content-Length"));
+        System.out.println("Content-Length: " + size);
+        System.out.println("i: " + i);
+        if (size > lastSize) {
+          lastSize = size;
+          Thread.sleep(1500);
+        } else if (size == lastSize) {
+          System.out.println("Content-Length: " + size);
+          System.out.println("exit: " + exit);
+          exit--;
+          Thread.sleep(1000);
+        }
+        if (exit < 0) {
+          System.out.println("video ok!");
+          return url.openStream();
+        }
+      } catch (Exception e) {
+        System.out.println("getSelenoidVideo exception: " + e.getMessage());
+        //e.printStackTrace();
+      }
+    }
+
+    return null;
+  }
+
+  @Step
+  public static void deleteSelenoidVideo(URL url) {
+    try {
+      HttpURLConnection deleteConn = (HttpURLConnection) url.openConnection();
+      deleteConn.setDoOutput(true);
+      deleteConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+      deleteConn.setRequestMethod("DELETE");
+      deleteConn.connect();
+      System.out.println("deleteSelenoidVideo response");
+      System.out.println(deleteConn.getResponseCode());
+      System.out.println(deleteConn.getResponseMessage());
+      deleteConn.disconnect();
+    } catch (IOException e) {
+      System.out.println("deleteSelenoidVideo exception");
+      e.printStackTrace();
+    }
+  }
+
+  public static String getSessionId() {
+    return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
   }
 
 }
