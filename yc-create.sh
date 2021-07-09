68b19d2621064c0b5181d44d537da18c6b7f396d
Aaftakhov <simple1elf@gmail.com>
2021-07-09 19:00:09 +0500

    test own s3 selenoid

--- yc-create.sh
+++ yc-create.sh
@@ -8,6 +8,7 @@ source "/var/jenkins_home/.bashrc"
 # shellcheck disable=SC2046
 echo $(yc compute instance create-with-container \
 --docker-compose-file docker-compose.yml \
+--container-env-file s3.env \
 --zone ru-central1-c \
 --public-ip \
 --memory $MEMORY --cores $CORE --core-fraction 100 \
