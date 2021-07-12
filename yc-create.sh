46572345519e82f99776142cdb6cc6adbb3dace0
Aaftakhov <simple1elf@gmail.com>
2021-07-12 22:07:38 +0500

    subnet

--- yc-create.sh
+++ yc-create.sh
@@ -9,8 +9,10 @@ source "/var/jenkins_home/.bashrc"
 echo $(yc compute instance create-with-container \
 --docker-compose-file docker-compose.yml \
 --memory $MEMORY --cores $CORE --core-fraction 100 \
+--zone ru-central1-c \
+--network-interface subnet-id=b0c0itboa01ughk2ggl4 \
 --format json)
 
 #--public-ip \
-#--zone ru-central1-c \
+
 #--service-account-name selenoid \
