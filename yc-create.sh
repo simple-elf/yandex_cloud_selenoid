4778673cce073e789b26127ee5f7ac292f2070ef
Aaftakhov <simple1elf@gmail.com>
2021-07-16 16:10:49 +0500

    wan subnet 2

--- yc-create.sh
+++ yc-create.sh
@@ -10,7 +10,7 @@ echo $(yc compute instance create-with-container \
 --docker-compose-file docker-compose.yml \
 --memory $MEMORY --cores $CORE --core-fraction 100 \
 --zone ru-central1-c \
---network-interface subnet-id=b0cpom0kvo0583snbohv \
+--network-interface subnet-id=b0chs6mi29ivgii84ojt \
 --format json)
 
 #--public-ip \
